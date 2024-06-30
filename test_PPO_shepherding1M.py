2057ea880a84adcd481dfc1b1aed0ea6847f8060
stefanocovone <stefanocovone@gmail.com>
2024-06-30 13:04:54 +0200

    Update test_PPO_shepherding1M.py

--- test_PPO_shepherding1M.py
+++ test_PPO_shepherding1M.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
                     num_validation_episodes=1000,
                     ent_coef=0.00,
                     anneal_lr=False,
-                    num_envs=128,
+                    num_envs=64,
                     )
 
         agent.train()
