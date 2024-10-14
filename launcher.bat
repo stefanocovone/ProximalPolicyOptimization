2343e21efc408670883513e6692e9cd041f244e4
stefanocovone <stefanocovone@gmail.com>
2024-10-14 22:25:06 +0200

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -5,13 +5,13 @@ echo RUNNING EXPERIMENT 1
 python test_PPO_shepherding1M.py --k_R 0.01 --k_p 1 --k_all 0 --k_chi 0.0 --exp_tag "test3_1" --l_r 0.0003
 
 echo RUNNING EXPERIMENT 2
-python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.1 --exp_tag "test_5" --lr 0.0003
+python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.2 --exp_tag "test_5" --lr 0.0003
 
 echo RUNNING EXPERIMENT 3
-python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.1 --exp_tag "test_5" --lr 0.0002
+python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.2 --exp_tag "test_5" --lr 0.0002
 
 echo RUNNING EXPERIMENT 4
-python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.1 --exp_tag "test_5" --lr 0.0001
+python test_PPO_shepherding1M.py --k_R 0.01 --k_p 0 --k_all 0.0 --k_chi 0.2 --exp_tag "test_5" --lr 0.0001
 
 REM Done
 echo All experiments have been launched.
